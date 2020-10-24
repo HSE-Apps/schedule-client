d0bb3371ce67cf3dcea2e6f5f6e7a273fd61a459
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2020-10-23 22:17:58 -0500

    Boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {Route, BrowserRouter as Router,} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <Router>
+      <Route path="/"/>
+    </Router>
+  );
+}
+
+export default App;
