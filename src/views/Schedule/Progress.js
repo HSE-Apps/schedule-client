3c3873f5f4fa52030f1050f2bd318619a8b33bbe
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2020-10-24 01:17:30 -0500

    Progress Component Basics

--- src/views/Schedule/Progress.js
+++ src/views/Schedule/Progress.js
@@ -1,127 +1,106 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 
-import {Progress, Typography, Card, Radio} from 'antd'
+import {Progress, Typography} from 'antd'
+
+import dayjs from 'dayjs'
+
+var duration = require('dayjs/plugin/duration')
+
+
+dayjs.extend(duration)
 
 const {Text} = Typography
 
-const scheduleData = {
-    1: [[7, 35, 8, 15, "Period 1"], [8, 15, 8, 22, "Going to Period 2"], [19, 30, 20, 30, "Period 2"], [11, 20, 11, 25, "Going to Period 3"], [11, 27, 11, 30, "Period 3"]],
-    2: [[7, 35, 8, 15, "Period 1"], [8, 15, 8, 22, "Going to Period 2"], [8, 22, 9, 2, "Period 2"], [9, 2, 9, 9, "Going to Period 3"], [9, 9, 9, 49, "Period 3"]],
-    3: [[7, 35, 8, 15, "Period 1"], [8, 15, 8, 22, "Going to Period 2"], [19, 30, 19, 55, "Period 2"], [23, 20, 23, 26, "Going to Period 3"], [23, 26, 23, 32, "Period 3"]],
-    4: [[7, 35, 8, 15, "Period 1"], [8, 15, 8, 22, "Going to Period 2"], [19, 30, 19, 55, "Period 2"], [23, 20, 23, 40, "Going to Period 3"], [23, 40, 23, 200, "Period 3"]],
-    6: [[0, 0, 0, 120, "Period 1"], [8, 15, 8, 22, "Period 2"], [8, 22, 9, 2, "Period 2"], [9, 2, 9, 9, "Going to Period 3"], [9, 9, 9, 49, "Period 3"]],
-  }
-  
-  
-  const ProgressDisplay = () => {
-  
-    const [schedule, setSchedule] = useState()
-    const [time, setTime] = useState()
-  
-    useEffect(() => {
-  
-      setInterval(() => {
-        setTime(new Date())
-      }, 1000)
-  
-      if (!schedule) {
-        setSchedule(scheduleData[(new Date()).getDay()])
-      }
-    }, [schedule])
-    
-    const radioStyle = {
-      display: 'block',
-      height: '30px',
-      lineHeight: '30px',
+const ProgressSchedule = ({schedule}) => {
+
+    const [period, setPeriod] = useState(null)
+    const [percent, setPercent] = useState(null)
+    const [text, setText] = useState(null)
+
+
+    const getPeriod = () => {
+
+        let currentTime = dayjs().valueOf()
+        let beforeSchool = currentTime < schedule[0].startTimeUnix
+        let afterSchool = currentTime > schedule[schedule.length - 1].endTimeUnix
+
+        if(beforeSchool){
+            console.log('Before School')
+        } else if (afterSchool) {
+            console.log('after school')
+        }
+
+        schedule.forEach(period => {
+            if(currentTime >= period.startTimeUnix && currentTime < period.endTimeUnix){
+                setPeriod(period)
+            }
+        })
     }
-    const periodReturn = (t) => {
-      console.log("checking periods")
-      var hr = t.getHours() 
-      var min = t.getMinutes() 
-      var sec = t.getSeconds()
-      for (const x in schedule) {
-        var currentTime = ((sec/3600.0)+ (min/60.0) + hr)
-        var startTime = ((schedule[x][1]/60.0)+schedule[x][0])
-        var endTime = ((schedule[x][3]/60.0)+schedule[x][2])   
-        if (startTime <= currentTime && currentTime <= endTime ) {
-          return (
-            <div>  
-              
-              <div style={{width:"500", marginRight:"4%", display:"flex", justifyContent:"center"}}>
-                <Progress 
-                  width={500}
-                  type="circle"
-                  strokeWidth="3.5"
-                  strokeColor={{
+
+    const setPercentAndText = () => {
+
+        let currentTime = dayjs().valueOf()
+
+        let range = period.endTimeUnix - period.startTimeUnix
+
+        let diffFromStart = currentTime - period.startTimeUnix
+
+        let diffFromEnd = period.endTimeUnix - currentTime 
+
+        setPercent((diffFromStart / range) * 100)
+
+        let minutesLeft = dayjs.duration(diffFromEnd, 'ms').minutes()
+
+        let secondsLeft = dayjs.duration(diffFromEnd, 'ms').seconds()
+
+        
+
+        setText(`${minutesLeft}:${secondsLeft > 9 ? secondsLeft : "0" + secondsLeft}`)
+
+    }
+
+
+    const updateFunc = () => {
+        getPeriod()
+        setPercentAndText()
+
+        setTimeout(() => updateFunc(), 100)
+    }
+
+    useEffect(() => {
+
+        if(period){
+            updateFunc()
+        } else {
+            getPeriod()
+        }
+    })
+
+
+
+    return(
+        <>  
+            <h1>{period && period.periodName}</h1>
+
+            <Progress
+                width={500}
+                type="circle"
+                format={() => 
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    {text || 'loading'}
+                    <Text type="secondary" style={{fontSize: "24px", marginTop: "10px", wordSpacing: "3px"}}>Until Period 5</Text>
+                </div>
+                }
+                strokeWidth="3.5"
+                strokeColor={{
                     '10%': '#1890FF',
                     '100%': '#eb2f96',
-                  }}
-                  percent={100-100*(endTime - currentTime)/(endTime - startTime)} 
-                  format={() => {
-                    var diff = endTime - currentTime
-                    return (
-                      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                        <div>
-                          {Math.floor(diff) === 0? <span></span>: <span>{Math.floor(diff)}:</span>}
-                          {Math.floor((diff*60)%60) < 10? <span>0</span>: <span></span>}
-                          {Math.floor((diff*60)%60)}:
-                          {Math.floor((diff*3600)%60) < 10? <span>0</span>: <span></span>}
-                          {Math.floor((diff*3600)%60)}
-                        </div>
-                        <Text type="secondary" style={{fontSize: "24px", marginTop: "10px", wordSpacing: "3px"}}>
-                          {schedule[(parseInt(x)+1).toString()]? 
-                            <div style={radioStyle}>Until {schedule[(parseInt(x)+1).toString()][4]}</div>
-                          :
-                            <div style={radioStyle}>Until School Ends</div>
-                          }
-                        </Text>
-                      </div>
-                    )
-                  }}
-                />
-              </div>
-              <div style={{width:"23%", display:"flex", justifyContent:"center"}}>
-                <Radio.Group
-                  style={{margin:"5%"}}
-                  size="small"
-                  onChange={(e) => { setSchedule(e[(new Date()).getDay()]) }}
-                >
-                  <Radio style={radioStyle} value={scheduleData}>A Lunch</Radio>
-                  <Radio style={radioStyle} value={scheduleData}>B Lunch</Radio>
-                  <Radio style={radioStyle} value={scheduleData}>C Lunch</Radio>
-                </Radio.Group>
-              </div>
-              <div style={{width:"27%"}}>
-                <div style={{margint:"5%"}}>
-                  {/* <div>Time: {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}</div> */}
-                  <div style={radioStyle}>Now: {schedule[x][4]}</div>
-                  <div style={radioStyle}>Ends at {schedule[x][2]}:{schedule[x][3]}</div>
-                  {schedule[(parseInt(x)+1).toString()]? 
-                    <div style={radioStyle}>Next: {schedule[(parseInt(x)+1).toString()][4]}</div>
-                  :
-                    <div style={radioStyle}>Next: None</div>
-                  }
-                </div>
-              </div>
-  
-            </div>
-          )
-        }
-      }
-      return (
-        <div>School is over!</div>
-      )
-    }
-  
-    return (
-      <div style={{margin:"10%"}}>
-        {time?
-          <div>{periodReturn(time)}</div>  
-        :
-          <div>rip</div>
-        }
-      </div>
+                }}
+                percent={percent || 0}
+            />
+            
+        </>
     )
-  }
-  
-  export default ProgressDisplay;
+}
+
+export default ProgressSchedule
--- src/views/Schedule/Schedule.js
+++ src/views/Schedule/Schedule.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
 import Progress from './Progress'
 
@@ -12,19 +12,92 @@ var customParseFormat = require('dayjs/plugin/customParseFormat')
 dayjs.extend(customParseFormat)
 
 
+const mockData = [
+    {
+        periodName: "Period 1",
+        startTime: "12:00 AM",
+        endTime: "12:15 AM"
+    }
+    ,
+    {
+        periodName: "Period 2",
+        startTime: "12:15 AM",
+        endTime: "12:20 AM"
+    }
+    ,
+    {
+        periodName: "Period 3",
+        startTime: "12:20 AM",
+        endTime: "1:03 AM"
+    },
+    {
+        periodName: "Period 4",
+        startTime: "1:03 AM",
+        endTime: "1:05 AM"
+    },
+    {
+        periodName: "Period 5",
+        startTime: "1:05 AM",
+        endTime: "1:06 AM"
+    },
+    {
+        periodName: "Period 6",
+        startTime: "1:06 AM",
+        endTime: "1:07 AM"
+    },
+    {
+        periodName: "Period 8",
+        startTime: "1:07 AM",
+        endTime: "1:11 AM"
+    },
+    {
+        periodName: "Period 8",
+        startTime: "1:11 AM",
+        endTime: "1:12 AM"
+    }
+]
+
+
 const Schedule = () => {
 
+    const [schedule, setSchedule] = useState(null)
+
+    const fetchSchedule = async () => {
+
+        let fetchedSchedule = await mockData
+
+        let scheduleWithUnix = []
+
+        fetchedSchedule.forEach(period => {
+            scheduleWithUnix.push({
+                ...period,
+                startTimeUnix: dayjs(period.startTime, 'h mm A').valueOf(),
+                endTimeUnix: dayjs(period.endTime, 'h mm A').valueOf(),
+            })
+        })
+
+
+        setSchedule(scheduleWithUnix)
+
+
+
+    }
+
     useEffect(() => {
-        let temp = dayjs('11:34 PM', 'h mm A').unix()
-        let bruh = dayjs().unix()
-        console.log(temp -bruh)
+        if(!schedule){
+            fetchSchedule()
+        }
     })
 
     return(
         <>
         <Navbar/>
         <div style={{background: "#fafcff", display:"flex",flexDirection:"row",height:"calc(100vh - 60px)", width: "100%", alignItems: 'center', justifyContent: 'center'}}>
-            <Progress/>
+            {schedule ?
+            <Progress schedule={schedule}/>
+            :
+            <>loading</>
+            }
         </div>
 
         </>
