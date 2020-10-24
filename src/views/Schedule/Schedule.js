8de6da4aae18c321f7dd58a847dde57f8a9a3dbf
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2020-10-23 23:57:54 -0500

    DayJS

--- src/views/Schedule/Schedule.js
+++ src/views/Schedule/Schedule.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import Progress from './Progress'
 
 import Navbar from '../Shared/Navbar'
 
+import dayjs from 'dayjs'
+
+
+var customParseFormat = require('dayjs/plugin/customParseFormat')
+
+dayjs.extend(customParseFormat)
+
+
 const Schedule = () => {
+
+    useEffect(() => {
+        let temp = dayjs('11:34 PM', 'h mm A').unix()
+        let bruh = dayjs().unix()
+        console.log(temp -bruh)
+    })
 
     return(
         <>
