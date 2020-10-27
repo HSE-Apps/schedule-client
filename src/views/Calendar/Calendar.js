0d7cca4465954e20403c992fea81ce39f3911726
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2020-10-27 02:13:08 -0400

    Calendar

--- src/views/Calendar/Calendar.js
+++ src/views/Calendar/Calendar.js
@@ -12,9 +12,9 @@ import useMedia from '../../hooks/useMedia'
 const {Text, Title} = Typography
 
 let mockData = {
-    noSchool: {
-        October: []
-    }
+    noSchool: ["19", "20","21","22","23"],
+    redWednesday: ["7", "28"],
+    blueWednesday: ["14"]
 }
 
 const CalendarSchedule = () => {
@@ -36,7 +36,7 @@ const CalendarSchedule = () => {
                     <div style={{display: 'flex', width: "100%", justifyContent: 'space-between', background: '#fafcff'}}>
                         <Title level={mobile ? 3 : 2} style={{color: "#333", fontWeight: "400"}}>{value.format('MMMM')} {value.format('Y')}</Title>
 
-                        <div style={{display: 'flex', justifyContent: 'space-between', width: '50px'}}>
+                        {/* <div style={{display: 'flex', justifyContent: 'space-between', width: '50px'}}>
                             <LeftOutlined onClick={() => {
                                 let temp = value.clone()
 
@@ -73,7 +73,7 @@ const CalendarSchedule = () => {
                                   onChange(temp)
                                 }} style={{fontSize: "20px"}}
                             />
-                        </div>
+                        </div> */}
                             
                         
                     </div>
@@ -91,7 +91,7 @@ const CalendarSchedule = () => {
                             return <></>
                         }
 
-                        if(dayOfWeek == "Sunday" || dayOfWeek == "Saturday" ){
+                        if(mockData.noSchool.includes(dayOfMonth) || (dayOfWeek == "Sunday" || dayOfWeek == "Saturday")){
                             return(
                                 <>
                                     <div className="calendar-box" style={{width: "100%", height: mobile ? "40px" : "100px", background: "#fafafa"}}>
@@ -101,14 +101,14 @@ const CalendarSchedule = () => {
                                 </>
 
                             )
-                        } else if(dayOfWeek == "Monday" || dayOfWeek == "Tuesday"){
+                        } else if(mockData.redWednesday.includes(dayOfMonth) || (dayOfWeek == "Monday" || dayOfWeek == "Tuesday")){
                             return(
                                 <div className="calendar-box" style={{width: "100%",height: mobile ? "40px" : "100px", color: "#69c0ff", background: "#f2fbff"}}>
                                         <Divider style={{marginBottom: "5px", marginTop: "10px", background: "#69c0ff", opacity: "0.15"}}/>
                                         <Text style={{paddingRight: "12px", color: "#69c0ff"}}>{dayOfMonth}</Text>
                                 </div>
                             )
-                        }else if(dayOfWeek == "Thursday" || dayOfWeek == "Friday"){
+                        }else if(mockData.blueWednesday.includes(dayOfMonth) || (dayOfWeek == "Thursday" || dayOfWeek == "Friday")){
                             return(
                                 <div className="calendar-box" style={{width: "100%", height: mobile ? "40px" : "100px", color: "#ff7875", background: "#fff5f4"}}>
                                         <Divider style={{marginBottom: "5px", marginTop: "10px", background: "#ff7875", opacity: "0.15"}}/>
