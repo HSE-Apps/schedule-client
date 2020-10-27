5545a8992b82a8180568fbc71dda00a6c3a96c85
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2020-10-27 02:39:35 -0400

    Periods

--- src/views/Schedule/Periods.js
+++ src/views/Schedule/Periods.js
@@ -17,7 +17,7 @@ const Periods = ({periods}) => {
     return(
         periods.map((period) => {
             if(period.periodName != "Passing Period"){
-               return( <motion.div whileHover={{x: 10}}  style={{boxShadow: " 2px 2px 15px rgb(0,118,220,0.18) ", width: "80%",maxWidth: "500px", height: "70px", borderRadius: "10px", cursor: 'pointer', display: "flex", justifyContent: "space-between", alignItems: "center", margin: "20px", padding: "24px"}}>
+               return( <motion.div whileHover={{x: 10}}  style={{boxShadow: " 2px 2px 15px rgb(0,118,220,0.18) ", width: "80%",maxWidth: "500px", height: mobile ? "70px" : "80px", borderRadius: "10px", cursor: 'pointer', display: "flex", justifyContent: "space-between", alignItems: "center", margin: "20px", padding: "24px"}}>
                 <div>
                     <Title level={mobile ? 4 : 3} style={{color: "#333", marginBottom: '0px'}}>{period.periodName}</Title>
                 </div>
