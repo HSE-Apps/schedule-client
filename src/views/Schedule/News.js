4926a587caec74e9235264ca5099d49916a22f87
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2020-10-28 17:53:49 -0400

    _blank target news

--- src/views/Schedule/News.js
+++ src/views/Schedule/News.js
@@ -88,7 +88,7 @@ const Periods = () => {
                 const date = dayjs(cast.date)
 
                 return(
-                        <a style={{width: "80%",maxWidth: "500px", display: 'block'}} href={cast.link}>
+                        <a target="_blank" style={{width: "80%",maxWidth: "500px", display: 'block'}} href={cast.link}>
                         <motion.div whileHover={{x: 10}} variants={periodV} style={{boxShadow: " 2px 2px 15px rgb(0,118,220,0.18) ", width: "100%",height: vh *  0.08, maxHeight: "80px", borderRadius: "10px", cursor: 'pointer', display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: vh * .02, padding: "24px"}}>
                             <div>
                                 <Title level={mobile ? 4 : 3} style={{color: "#333", marginBottom: '0px'}}>{cast.title.rendered.replace(';', ':').split(':')[0].split('&')[0] }</Title>
