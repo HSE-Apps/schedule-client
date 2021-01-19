21a7565fb3f57026776bb14c35d83d96a0344cca
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2021-01-19 08:18:31 -0500

    schedule

--- src/views/Schedule/tempData.js
+++ src/views/Schedule/tempData.js
@@ -207,81 +207,85 @@ export let specialSchedule = [
     {
         periodName: "Period 1",
         startTime: "7:30 AM",
-        endTime: "8:12 AM",
+        endTime: "8:13 AM",
     }, {
         periodName: "Passing Period",
-        startTime: "8:12 AM",
-        endTime: "8:22 AM",
+        startTime: "8:13 AM",
+        endTime: "8:20 AM",
         isPassing: true
     },
     {
         periodName: "Period 2",
-        startTime: "8:22 AM",
-        endTime: "9:03 AM",
+        startTime: "8:20 AM",
+        endTime: "9:02 AM",
     },
     {
         periodName: "Passing Period",
-        startTime: "9:03 AM",
-        endTime: "9:13 AM",
+        startTime: "9:02 AM",
+        endTime: "9:09 AM",
         isPassing: true
     },
     {
         periodName: "Period 3",
-        startTime: "9:13 AM",
-        endTime: "9:54 AM",
+        startTime: "9:09 AM",
+        endTime: "9:51 AM",
     },
     {
         periodName: "Passing Period",
-        startTime: "9:54 AM",
-        endTime: "10:04 AM",
+        startTime: "9:51 AM",
+        endTime: "9:58 AM",
         isPassing: true
     },
     {
         periodName: "Period 4",
-        startTime: "10:04 AM",
-        endTime: "10:45 AM",
+        startTime: "9:58 AM",
+        endTime: "10:40 AM",
     },
     {
         periodName: "Passing Period",
-        startTime: "10:45 AM",
-        endTime: "10:55 AM",
+        startTime: "10:40 AM",
+        endTime: "10:40 AM",
         isPassing: true
     },
     {
         periodName: "Period 5",
-        startTime: "10:55 AM",
-        endTime: "11:36 AM",
-    },
-    {
+        startTime: "10:40 AM",
+        endTime: "12:20 PM",
+        lunchPeriods: {
+            A: {
+                startTime: "10:40 AM",
+                endTime: "11:11 AM"
+            },
+            B: {
+                startTime: "11:15 AM",
+                endTime: "11:45 AM"
+            },
+            C: {
+                startTime: "11:50 AM",
+                endTime: "12:20 PM"
+            },
+        }
+    }, {  {
         periodName: "Passing Period",
-        startTime: "11:36 AM",
-        endTime: "11:36 AM",
-        isPassing: true
-    }, {
-        periodName: "Lunch Break",
-        startTime: "11:36 AM",
-        endTime: "12:16 PM",
-    },  {
-        periodName: "Passing Period",
-        startTime: "12:16 PM",
-        endTime: "12:26 PM",
+        startTime: "12:20 PM",
+        endTime: "12:27 PM",
         isPassing: true
     },
 
     {
         periodName: "Period 6",
-        startTime: "12:26 PM",
-        endTime: "1:08 PM",
+        startTime: "12:27 PM",
+        endTime: "1:09 PM",
     },
     {
         periodName: "Passing Period",
-        startTime: "1:08 PM",
-        endTime: "1:18 PM",
+        startTime: "1:09 PM",
+        endTime: "1:16 PM",
         isPassing: true
     },
     {
         periodName: "Period 7",
-        startTime: "1:18 PM",
+        startTime: "1:16 PM",
         endTime: "2:00 PM",
     },
 
@@ -299,22 +303,3 @@ export let weekendSchedule = [
 
 
 
-// {
-//     periodName: "Period 3",
-//     startTime: "10:30 AM",
-//     endTime: "12:29 PM",
-//     lunchPeriods: {
-//         A: {
-//             startTime: "10:34 AM",
-//             endTime: "10:57 AM"
-//         },
-//         B: {
-//             startTime: "11:13 AM",
-//             endTime: "11:43 AM"
-//         },
-//         C: {
-//             startTime: "11:59 AM",
-//             endTime: "12:29 PM"
-//         },
-//     }
-// }, {
