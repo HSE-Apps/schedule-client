3c51002641f5a6c4ef7be9b989b1bed5e17d826c
ArnavMeh <55332253+ArnavMeh@users.noreply.github.com>
2021-01-20 09:52:04 -0500

    wed change - change after today
    prob doesnt work lol

--- src/views/Schedule/tempData.js
+++ src/views/Schedule/tempData.js
@@ -36,7 +36,7 @@ export let tuesFridSchedule = [
         startTime: "11:05 AM",
         endTime: "11:45 AM",
     },
-    , {
+    {
         periodName: "Passing Period",
         startTime: "11:45 AM",
         endTime: "11:55 AM",
@@ -47,7 +47,7 @@ export let tuesFridSchedule = [
         startTime: "11:55 AM",
         endTime: "1:15 PM",
     },
-    , {
+    {
         periodName: "Passing Period",
         startTime: "1:15 PM",
         endTime: "1:20 PM",
@@ -117,88 +117,142 @@ export let monThurSchedule = [
     
 ]
 
+// export let wedSchedule = [
+//     {
+//         periodName: "Period 1",
+//         startTime: "7:30 AM",
+//         endTime: "8:12 AM",
+//     }, {
+//         periodName: "Passing Period",
+//         startTime: "8:12 AM",
+//         endTime: "8:22 AM",
+//         isPassing: true
+//     },
+//     {
+//         periodName: "Period 2",
+//         startTime: "8:22 AM",
+//         endTime: "9:03 AM",
+//     },
+//     {
+//         periodName: "Passing Period",
+//         startTime: "9:03 AM",
+//         endTime: "9:13 AM",
+//         isPassing: true
+//     },
+//     {
+//         periodName: "Period 3",
+//         startTime: "9:13 AM",
+//         endTime: "9:54 AM",
+//     },
+//     {
+//         periodName: "Passing Period",
+//         startTime: "9:54 AM",
+//         endTime: "10:04 AM",
+//         isPassing: true
+//     },
+//     {
+//         periodName: "Period 4",
+//         startTime: "10:04 AM",
+//         endTime: "10:45 AM",
+//     },
+//     {
+//         periodName: "Passing Period",
+//         startTime: "10:45 AM",
+//         endTime: "10:55 AM",
+//         isPassing: true
+//     },
+//     {
+//         periodName: "Period 5",
+//         startTime: "10:55 AM",
+//         endTime: "11:36 AM",
+//     },
+//     {
+//         periodName: "Passing Period",
+//         startTime: "11:36 AM",
+//         endTime: "11:36 AM",
+//         isPassing: true
+//     }, {
+//         periodName: "Lunch Break",
+//         startTime: "11:36 AM",
+//         endTime: "12:16 PM",
+//     },  {
+//         periodName: "Passing Period",
+//         startTime: "12:16 PM",
+//         endTime: "12:26 PM",
+//         isPassing: true
+//     },
+
+//     {
+//         periodName: "Period 6",
+//         startTime: "12:26 PM",
+//         endTime: "1:08 PM",
+//     },
+//     {
+//         periodName: "Passing Period",
+//         startTime: "1:08 PM",
+//         endTime: "1:18 PM",
+//         isPassing: true
+//     },
+//     {
+//         periodName: "Period 7",
+//         startTime: "1:18 PM",
+//         endTime: "2:00 PM",
+//     },
+
+// ]
 export let wedSchedule = [
+
+
     {
         periodName: "Period 1",
         startTime: "7:30 AM",
-        endTime: "8:12 AM",
+        endTime: "8:55 AM",
     }, {
         periodName: "Passing Period",
-        startTime: "8:12 AM",
-        endTime: "8:22 AM",
+        startTime: "8:55 AM",
+        endTime: "9:02 AM",
         isPassing: true
     },
     {
         periodName: "Period 2",
-        startTime: "8:22 AM",
-        endTime: "9:03 AM",
+        startTime: "9:02 AM",
+        endTime: "10:27 AM",
     },
     {
         periodName: "Passing Period",
-        startTime: "9:03 AM",
-        endTime: "9:13 AM",
+        startTime: "10:27 AM",
+        endTime: "10:27 AM",
         isPassing: true
     },
     {
         periodName: "Period 3",
-        startTime: "9:13 AM",
-        endTime: "9:54 AM",
-    },
-    {
+        startTime: "10:27 AM",
+        endTime: "12:29 PM",
+        lunchPeriods: {
+            A: {
+                startTime: "10:27 AM",
+                endTime: "10:57 AM"
+            },
+            B: {
+                startTime: "11:13 AM",
+                endTime: "11:43 AM"
+            },
+            C: {
+                startTime: "11:59 AM",
+                endTime: "12:29 PM"
+            },
+        }
+    }, {
         periodName: "Passing Period",
-        startTime: "9:54 AM",
-        endTime: "10:04 AM",
+        startTime: "12:29 AM",
+        endTime: "12:36 AM",
         isPassing: true
-    },
+    }, 
     {
         periodName: "Period 4",
-        startTime: "10:04 AM",
-        endTime: "10:45 AM",
-    },
-    {
-        periodName: "Passing Period",
-        startTime: "10:45 AM",
-        endTime: "10:55 AM",
-        isPassing: true
-    },
-    {
-        periodName: "Period 5",
-        startTime: "10:55 AM",
-        endTime: "11:36 AM",
-    },
-    {
-        periodName: "Passing Period",
-        startTime: "11:36 AM",
-        endTime: "11:36 AM",
-        isPassing: true
-    }, {
-        periodName: "Lunch Break",
-        startTime: "11:36 AM",
-        endTime: "12:16 PM",
-    },  {
-        periodName: "Passing Period",
-        startTime: "12:16 PM",
-        endTime: "12:26 PM",
-        isPassing: true
-    },
-
-    {
-        periodName: "Period 6",
-        startTime: "12:26 PM",
-        endTime: "1:08 PM",
-    },
-    {
-        periodName: "Passing Period",
-        startTime: "1:08 PM",
-        endTime: "1:18 PM",
-        isPassing: true
-    },
-    {
-        periodName: "Period 7",
-        startTime: "1:18 PM",
+        startTime: "12:36 PM",
         endTime: "2:00 PM",
-    },
-
+    }
 ]
 
 
