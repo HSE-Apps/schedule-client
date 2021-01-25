f3ae7d61a71c7cb87e1ff72051e05d1a9d80899d
Aryadeep Buddha <40647186+AryaBuddha@users.noreply.github.com>
2021-01-25 07:40:14 -0500

    Fixed Schedule dates

--- src/views/Schedule/Calendar.js
+++ src/views/Schedule/Calendar.js
@@ -12,8 +12,8 @@ import useMedia from '../../hooks/useMedia'
 const {Text, Title} = Typography
 
 let mockData = {
-    redMon: ["1","2","3","4","5"],
-    blueMon: [],
+    redMon: ["1","3","5"],
+    blueMon: ["2", "4"],
     noSchool: ["18"]
 }
 
